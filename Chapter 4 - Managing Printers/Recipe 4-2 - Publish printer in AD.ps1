27de69bcb2b72860127ed3a85ddf5754bcd0e8a0
Thomas Lee <doctordns@gmail.com>
2017-10-16 19:30:22 +0100

    Added Recipe 4-2

--- Chapter 4 - Managing Printers/Recipe 4-2 - Publish printer in AD.ps1
+++ Chapter 4 - Managing Printers/Recipe 4-2 - Publish printer in AD.ps1
@@ -1,25 +1,15 @@
-﻿# Chapter 4 - Managing Printers
-# Recipe 4-2 Publish printer to AD
-# Run on Print Server
+﻿# Recipe 4-2 Publishing a printer
 
-# 1. Find printer to publish 
-$Printer = Get-Printer -Name 'SGCP1'
+# 1. Get the printer to publish and store the returned object in $Printer:
+$Printer = Get-Printer -Name SGCP1
 
-# 2. Observe publication status
-$Printer | Format-Table Name, Published
+# 2. Observe the publication status:
+$Printer | Format-Table -Property Name, Published
 
-# 3. Publish the printer to AD
+# 3. Publish the printer to AD:
 $Printer | Set-Printer -Published $true `
-              -Location '10th floor 10E4'
+                       -Location '10th floor 10E4'
 
-# 4 Observe publication status
-Get-Printer -Name 'SGCP1' |
-     Format-Table Name, Published, Location
-
-
-
-
-
-<# remove publication - for testing
-Get-Printer -Name 'SGCP1' |Set-Printer -Published $false
-#>
+# 4. View the updated publication status:
+Get-Printer -Name SGCP1 |
+    Format-Table -Property Name, Published, Location
