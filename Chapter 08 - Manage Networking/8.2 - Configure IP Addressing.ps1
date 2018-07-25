243419a31472370cf9787ad5cdcaf5da495a6982
Thomas Lee <doctordns@gmail.com>
2018-07-25 21:26:33 +0100

    updating recipe 8-2

--- Chapter 08 - Manage Networking/8.2 - Configure IP Addressing.ps1
+++ Chapter 08 - Manage Networking/8.2 - Configure IP Addressing.ps1
@@ -1,26 +1,35 @@
-﻿# Recipe 10-2 - ConfigureIP addressing
+﻿# Recipe 8-2 - ConfigureIP addressing
 
 # 1 Get existing IP address information
 $IPType = "IPv4"
-$Adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
-$Interface = $Adapter | Get-NetIPInterface -AddressFamily $IPType
+$Adapter = Get-NetAdapter | 
+    Where-Object Status -eq "up"
+$Interface = $Adapter | 
+    Get-NetIPInterface -AddressFamily $IPType
 $IfIndex = $Interface.ifIndex
 $IfAlias = $Interface.Interfacealias
 Get-NetIPAddress -InterfaceIndex $Ifindex -AddressFamily $IPType
 
 # 2. Set the IP address for DC2
-New-NetIPAddress -InterfaceAlias $IfAlias `
-     -PrefixLength    24 `
-     -IPAddress      '10.10.10.11' `
-     -DefaultGateway '10.10.10.254' `
-     -AddressFamily   $IPType
+$IPHT = @{
+    InterfaceAlias = $IfAlias
+    PrefixLength   =  24
+    IPAddress      = '10.10.10.11'
+    DefaultGateway = '10.10.10.254'
+    AddressFamily  =  $IPType
+}
+New-NetIPAddress @IPHT
 
 # 3. Set DNS Server details
-Set-DnsClientServerAddress -InterfaceIndex 3 `
-     ServerAddresses 10.10.10.10
+$CAHT = @{
+    InterfaceIndex = 3 
+    ServerAddresses= '10.10.10.10'
+}
+
+Set-DnsClientServerAddress  @CAHT
 
 # 4. Test new configuration
 Get-NetIPAddress -InterfaceIndex $IfIndex
 Test-NetConnection -ComputerName DC1 
-Resolve-DnsName -Name dc2.reskit.org -Server DC1 | 
-    Where Type -eq 'A'
+Resolve-DnsName -Name dc2.reskit.org -Server DC1 |
+    Where-Object Type -eq 'A'
