849de949a400d42a67c041ee7a4dc80eb610a2b3
Thomas Lee <doctordns@gmail.com>
2018-07-25 16:12:14 +0100

    finish 7-5

--- Chapter 07 - Troubleshooting/Recipe 7-5 - Forward Event Log entries.ps1
+++ Chapter 07 - Troubleshooting/Recipe 7-5 - Forward Event Log entries.ps1
@@ -2,8 +2,8 @@
 
 # 1. Create the collector security group.
 $ECGName = 'Event Collection Group'
-New-ADGroup -Name $ECGName -GroupScope Global `
-            -Path 'OU=IT,DC=Reskit,DC=Org'
+$Path = 'OU=IT,DC=Reskit,DC=Org'
+New-ADGroup -Name $ECGName -GroupScope Global -Path  $Path
 Add-ADGroupMember -Identity $ECGName -Members 'DC1$'
 
 # 2. Get membership
@@ -11,19 +11,23 @@ Get-ADGroupMember -Identity $ECGName
 
 # 3. Create a new GPO on DC1 to configure event collection
 $sb = {
-  $GPOName = 'Event Collection'
-  $ECGName = 'Event Collection Group'
-  $gpo  = New-GPO -Name $GPOName
-  $link = New-GPLink -Name $GPOName `
-                     -Target "DC=Reskit,DC=Org"
-  $p1 = Set-GPPermission -Name $GPOName `
-                   -TargetName "$ECGName" `
-                   -TargetType Group `
-                   -PermissionLevel GpoApply
-  $p2 = Set-GPPermission -Name $GPOName `
-                 -TargetName 'Authenticated Users' `
-                 -TargetType Group `
-                 -PermissionLevel None }
+$GPOName = 'Event Collection'
+$ECGName = 'Event Collection Group'
+$gpo  = New-GPO -Name $GPOName
+$link = New-GPLink -Name $GPOName -Target "DC=Reskit,DC=Org"
+$GPHT1 = @{
+  Name            = $GPOName
+  TargetName      = $ECGName
+  TargetType      = 'Group'
+  PermissionLevel = 'GpoApply'
+}
+$p1 = Set-GPPermission @GPHT1
+$GPHT2 = @{
+    TargetName      = 'Authenticated Users'
+    TargetType      = 'Group'
+    PermissionLevel = 'None'
+}
+$p2 = Set-GPPermission @GPHT2
 Invoke-Command -ComputerName DC1 -ScriptBlock $sb
 
 # 4. Set GPO Permissions:
@@ -40,20 +44,21 @@ Set-GPRegistryValue -Name "Event Collector" -Key $WinRMKey `
 -ValueName "IPv4Filter" -Type STRING -Value "*"
 Set-GPRegistryValue -Name "Event Collector" -Key $WinRMKey `
 -ValueName "IPv6Filter" -Type STRING -Value "*"
-4. Enable and configure Windows Event Collector on the collector system.
+
+# 4. Enable and configure Windows Event Collector on the collector system.
 echo y | wecutil qc
-5. Create the XML file for the subscription, and save the file.
-Following is an example of the subscription.xml file used in this example:
-<?xml version="1.0" encoding="UTF-8"?>
+
+# 5. Create the XML file for the subscription, and save the file.
+# Following is an example of the subscription.xml file used in this example:
+
+    $S = '<?xml version="1.0" encoding="UTF-8"?>
 <Subscription xmlns="http://schemas.microsoft.com/2006/03/windows/
 events/subscription">
 <SubscriptionId>Collect security</SubscriptionId>
 <SubscriptionType>SourceInitiated</SubscriptionType>Troubleshooting Servers with PowerShell
-290
 <Description></Description>
 <Enabled>true</Enabled>
-<Uri>http://schemas.microsoft.com/wbem/wsman/1/windows/
-EventLog</Uri>
+<Uri>http://schemas.microsoft.com/wbem/wsman/1/windows/EventLog</Uri>
 <ConfigurationMode>Normal</ConfigurationMode>
 <Query>
 <![CDATA[
@@ -66,21 +71,20 @@ or Level=2 or Level=3)]]</Select></Query></QueryList>
 <ContentFormat>RenderedText</ContentFormat>
 <Locale Language="en-US"/>
 <LogFile>ForwardedEvents</LogFile>
-<PublisherName>Microsoft-Windows-EventCollector</
-PublisherName>
+<PublisherName>Microsoft-Windows-EventCollector</PublisherName>
 <AllowedSourceNonDomainComputers>
 <AllowedIssuerCAList>
 </AllowedIssuerCAList>
 </AllowedSourceNonDomainComputers>
-<AllowedSourceDomainComputers>O:NSG:BAD:P(A;;GA;;;DC)S:</
-AllowedSourceDomainComputers>
-</Subscription>
-A sample XML file can be viewed by executing wecutil cs /?.
-You can export an existing subscription to XML by executing
-wecutil gs <name> /f:XML.
-6. Create a subscription.
+<AllowedSourceDomainComputers>O:NSG:BAD:P(A;;GA;;;DC)S:
+</AllowedSourceDomainComputers>
+</Subscription>'
+$s | out-file -FilePath .\EL.xml
+wecutil gs .\EL.XML /f:XML.
+
+# 6. Create a subscription.
 wecutil cs subscription.xml
-7. Create the source security group.
+
+# 7. Create the source security group.
 New-ADGroup -Name "Event Source" -GroupScope Global
-Add-ADGroupMember -Identity "Event Source" -Members Server2$Chapter 9
-29
+Add-ADGroupMember -Identity "Event Source" -Members Server2
