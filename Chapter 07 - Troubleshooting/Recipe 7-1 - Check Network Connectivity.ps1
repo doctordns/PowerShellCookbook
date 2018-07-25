ef0987eb812c8309a37da08ef827b24022da3243
Thomas Lee <doctordns@gmail.com>
2018-07-25 15:57:13 +0100

    edit 7-1

--- Chapter 07 - Troubleshooting/Recipe 7-1 - Check Network Connectivity.ps1
+++ Chapter 07 - Troubleshooting/Recipe 7-1 - Check Network Connectivity.ps1
@@ -1,6 +1,6 @@
 ﻿#  Recipe 8-1 - Check connectivity
 
-# 1. Use Ping to test connectivity to Dc1
+# 1. Use Ping to test connectivity to DC1
 Ping DC1
 
 # 2. Use Test-NetConnnection to test connection to DC1
@@ -13,11 +13,15 @@ Test-Connection -ComputerName DC1 -Quiet
 Test-Connection -computer 'DC1','DC2','SRV1' -Count 1
 
 # 5. Test connectivity for SMB traffic
-Test-NetConnection -ComputerName DC1 -CommonTCPPort SMB 
+Test-NetConnection -ComputerName DC1 -CommonTCPPort SMB
 
 # 6. Get detailed connectivity check, using DC1 with HTTP
-Test-NetConnection -ComputerName DC1 -CommonTCPPort HTTP `
-                   -InformationLevel Detailed
+$TNCHT = @{
+    ComputerName     = 'DC1'
+    CommonTCPPort    = 'HTTP'
+    InformationLevel = 'Detailed'
+}
+ Test-NetConnection $TNCHT
 
 # 7. Look for a particular port (i.e LDAP on DC1)
 Test-NetConnection -ComputerName dc1 -Port 445
@@ -25,7 +29,8 @@ Test-NetConnection -ComputerName dc1 -Port 445
 # 8. Look for a host that does not exist
 Test-NetConnection -ComputerName 10.10.10.123
 
-# 9. Look for a host that exists but a port/application that does not exist:
+# 9. Look for a host that exists but a port/application
+#    that does not exist:
 Test-NetConnection -ComputerName DC1 -PORT 9999
 
 
