53e625164c8a921a6de9a80106f5b3d754b233bf
Thomas Lee <doctordns@gmail.com>
2018-07-25 22:00:33 +0100

    update 8.4

--- Chapter 08 - Manage Networking/8.4 - Install a DC and DNS.ps1
+++ Chapter 08 - Manage Networking/8.4 - Install a DC and DNS.ps1
@@ -1,35 +1,43 @@
-﻿# Recipe 10-4 Install Domain Controllers.
-# This recipe requires first DC1 (as a workgroup system), then DC2, a domain joined
-# computer.
+﻿# Recipe 8-4 Install Domain Controllers.
+# This recipe requires first DC1 (as a workgroup system),
+# then DC2, a domain joined computer.
 
 #  ON DC1
 
 # 1. Install the AD Domain Services
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
-# 2. Instal This system as the first DC in a new forest.
-$PasswordSS = ConvertTo-SecureString  -String 'Pa$$w0rd' -AsPlainText -Force
-Install-ADDSForest -DomainName Reskit.Org `
-                   -SafeModeAdministratorPassword $PasswordSS `
-                   -InstallDNS `
-                   -DomainMode Win2012R2 -ForestMode Win2012R2 `
-                   -Force -NoRebootOnCompletion
+# 2. Install This system as the first DC in a new forest.
+$PSS = ConvertTo-SecureString  -String 'Pa$$w0rd' -AsPlainText -Force
+$ADHT = @{
+  DomainName                    = 'Reskit.Org'
+  SafeModeAdministratorPassword = $PSS
+  InstallDNS                    = $true
+  DomainMode                    = 'Win2012R2'
+  ForestMode                    = 'Win2012R2'
+  Force                         = $true
+  NoRebootOnCompletion          = $true
+}
+Install-ADDSForest @ADHT
 Restart-Computer -Force
 
-# After installation the system reboots.
+### Second party of this script - run on DC2
 
-# 3. After the roboot, Logon to the DC and then do the following
-Get-ADDomain 
-Get-DNSServer -Computer DC1 
-Get-DNsServerZone -Computer DC1
+# 1. Check DC1 can be reached oer 556, 389 from DC2
+Resolve-DnsName -Name dc1.reskit.org -Server DC1.Reskit.Org -Type A
+Test-NetConnection -ComputerName DC1.Reskit.Org -Port 445
+Test-NetConnection -ComputerName DC1.Reskit.Org -Port 389
 
-# 4. Install a second DC - Logon to DC2, a domain joined sytstem as administrator
-
-
-
-#  removal 
-Uninstall-ADDSDomainController -LocalAdministratorPassword $PasswordSS -LastDomainControllerInDomain -NoRebootOnCompletion -force -RemoveApplicationPartitions
-
-
-
--LastDomainControllerInDomain
+# 2. Add the AD DS features on DC2
+$Features = 'AD-Domain-Services, DNS,RSAT-DHCP, Web-Mgmt-Tools'
+Install-WindowsFeature -Feature @Features
+# Promote DC2 to be a DC in the Reskit.Org domain:
+$PSS = ConvertTo-SecureString -String 'Pa$$w0rd' -AsPlainText -Force
+$IHT =@{
+  DomainName                    = 'Reskit.org'
+  SafeModeAdministratorPassword = $PSS
+  SiteName                      = 'Default-First-Site-Name'
+  NoRebootOnCompletion          = $true
+  Force                         =  $true
+}
+Install-ADDSDomainController @IHT
