9ac2fe046d06451b5dc001b2e3cb0193b6ec7392
Thomas Lee <doctordns@gmail.com>
2018-07-25 16:00:14 +0100

    edit 7-3

--- Chapter 07 - Troubleshooting/Recipe 7-3 - Use BPA.ps1
+++ Chapter 07 - Troubleshooting/Recipe 7-3 - Use BPA.ps1
@@ -1,27 +1,26 @@
-﻿# Recipe 8-3
+﻿# Recipe 8-3 - Using BPA
 
-# 1. Get all BPA Models on SRV1:
+# 1. Get all BPA Models on SRV1
 Get-BpaModel | Format-Table -Property Name, Id
 
 # 2. Invoke BPA model for file services
-Invoke-BpaModel -ModelId Microsoft/Windows/FileServices 
+Invoke-BpaModel -ModelId Microsoft/Windows/FileServices
 
 # 3. Get BPA results for this scan:
-$Results = Get-BpaResult `
-              -ModelId Microsoft/Windows/FileServices
+$Results = Get-BpaResult -ModelId Microsoft/Windows/FileServices
 
 # 4. Display how many tests/results in the BPA model
-$Results.count
+$Results.Count
 
 # 5 How many errors were found?
-($Results | Where-Object Severity -eq 'Error').count
+($Results | Where-Object Severity -eq 'Error').Count
 
-# 6. Hw many warnings were found?
+# 6. How many warnings were found?
 $Warnings = $Results | Where-Object Severity -eq 'Warning'
-$Warnings.count
+$Warnings.Count
 
 # 7. Examaine the first 3 warnings:
-$Warnings | Select -First 3 |
+$Warnings | Select-Object -First 3 |
     Format-List Category, Problem, Impact, Resolution
 
 # 8. Use BPA Remotely - what models exist on DC1
@@ -32,17 +31,16 @@ Invoke-Command -ComputerName DC1 -ScriptBlock {Get-BpaModel} |
 $SB = {Invoke-BpaModel -ModelId `
                     Microsoft/Windows/DirectoryServices}
 Invoke-Command -ComputerName DC1 -ScriptBlock $sb
-                
 
 # 10. Get the results
 $SB = {Get-BpaResult -ModelId Microsoft/WIndows/DirectoryServices}
-$RRRESULTS = Invoke-Command -ComputerName DC1 -ScriptBlock $sb
+$RRESULTS = Invoke-Command -ComputerName DC1 -ScriptBlock $sb
 
 # 11 How many checks/results?
 $RResults.count
-$RResults | Group-Object SEVERITY | 
+$RResults | Group-Object SEVERITY |
                  Format-Table -Property Name, Count
- 
+
 # 12. Look at one error:
 $RResults | Where-Object Severity -EQ 'Error' |
     Format-List -Property Category,Problem,Impact,Resolution
