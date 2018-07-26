ba28cffc5ec388947f24eea07db35dbf94392c45
Thomas Lee <doctordns@gmail.com>
2018-07-26 11:36:06 +0100

    fix file names

--- Chapter 08 - Manage Networking/8.5 - Installl and authorize dhcp serverr.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Recipe 10-6 - Setup Install and Authorise a DHCP server.
-# Run on DC1
-
-# 1. Install the DHCP Feature
-Install-WindowsFeature -Name DHCP `
-                   -IncludeManagementTools
-
